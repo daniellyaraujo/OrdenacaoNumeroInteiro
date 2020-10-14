ed2e595742460db6af401f654a95706d97043acf
Danielly Araujo <araaujodann@gmail.com>
2020-10-14 16:41:18 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OrdenacaoNumeroInteiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            var numero = Console.ReadLine();
+            var tamanho = numero.Length;
+            int numero1 = int.Parse(numero);
+
+            if (numero1 >= 10000000)
+            {
+                Console.Write("Seu número é: ");
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                int[] digitos = new int[tamanho];
+
+                for (int i = 0; i < tamanho; i++)
+                {
+                    digitos[i] = numero1 % 10;
+                    numero1 = numero1 / 10;
+                }
+
+                Array.Sort(digitos);
+                int valorDescendente = 0;
+                for (int i = tamanho - 1; i >= 0; i--)
+                {
+                    valorDescendente = valorDescendente * 10 + digitos[i];
+                }
+                Console.Write("Seu número é: ");
+                Console.WriteLine(valorDescendente);
+            }
+        }
+    }
+}
